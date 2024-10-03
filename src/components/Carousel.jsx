3c1c6c0cc1246457157d7b5b698eb8252a4ae43b
riddsep <ridoseptiawan00@gmail.com>
2024-10-03 13:54:56 +0700

    feat: add continue watching section

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,5 +1,29 @@
-const Carousel = () => {
-  return <h1>test</h1>;
-};
+import { useCallback } from "react";
+import useEmblaCarousel from "embla-carousel-react";
 
+export const Carousel = ({ children }) => {
+  const [emblaRef, emblaApi] = useEmblaCarousel();
+
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev();
+  }, [emblaApi]);
+
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext();
+  }, [emblaApi]);
+
+  return (
+    <div className="embla w-full ">
+      <div className="embla__viewport overflow-x-auto " ref={emblaRef}>
+        <div className="embla__container flex gap-4">{children}</div>
+        <button className="embla__prev hidden" onClick={scrollPrev}>
+          <img src="./assets/icons/arrow-left.svg" alt="" />
+        </button>
+        <button className="embla__next hidden" onClick={scrollNext}>
+          <img src="./assets/icons/arrow-right.svg" alt="" />
+        </button>
+      </div>
+    </div>
+  );
+};
 export default Carousel;
---
+++ src/components/Continues.jsx
@@ -0,0 +1,30 @@
+import Carousel from "./Carousel";
+import dataset from "../data/continues-film";
+import { Fragment } from "react";
+const Continues = () => {
+  const filmList = dataset.map((data) => {
+    return (
+      <Fragment key={data.id}>
+        <div className="flex basis-[309px] shrink-0 relative">
+          <img src={data.imageURL} alt="" className="w-full" />
+          <div className="text-white w-full p-4 text-sm font-semibold flex justify-between items-center absolute bottom-0">
+            {data.title}
+            <span className="flex items-center gap-1">
+              <img src="./assets/icons/star.svg" alt={data.title} />
+              {data.rating}/5
+            </span>
+          </div>
+        </div>
+      </Fragment>
+    );
+  });
+  return (
+    <section className="w-full bg-[#212121] p-5 relative">
+      <h1 className="text-white text-xl font-bold mb-5">
+        Melanjutkan Tonton Film
+      </h1>
+      <Carousel>{filmList}</Carousel>
+    </section>
+  );
+};
+export default Continues;
