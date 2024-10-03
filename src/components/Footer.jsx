c067daedd46a6e6aafbc789bed7c5c15bb443538
riddsep <ridoseptiawan00@gmail.com>
2024-10-04 00:09:25 +0700

    feat: add footer responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,25 @@
 const Footer = () => {
   return (
-    <footer className="w-full flex flex-col bg-[#212121] p-5  lg:px-20 lg:py-10 gap-10">
-      <div className="text-[#C1C2C4]">
+    <footer className="w-full flex flex-col lg:justify-between lg:flex-row bg-[#212121] p-5 lg:px-20 lg:py-[60px] gap-10">
+      <div className="text-[#C1C2C4] text-xs md:text-base">
         <img
           src="./assets/icons/logo.svg"
           alt="logo icon"
-          className="mb-3 text-xs"
+          className="mb-3 w-[163px]"
         />
         @2023 Chill All Rights Reserved.
       </div>
-      <div className="flex flex-col gap-3">
+      <div className="flex flex-col lg:flex-row gap-3 lg:gap-40 ">
         <div>
-          <h1 className="text-white text-base flex justify-between">
-            Genre <img src="./assets/icons/drop-icon.svg" alt="" />
+          <h1 className="text-white text-base flex justify-between lg:mb-3">
+            Genre{" "}
+            <img
+              src="./assets/icons/drop-icon.svg"
+              alt=""
+              className="lg:hidden"
+            />
           </h1>
-          <div className="hidden">
+          <div className="hidden lg:flex gap-7 cursor-pointer">
             <ul className="text-[#C1C2C4]">
               <li>Aksi</li>
               <li>Anak-anak</li>
@@ -40,10 +45,15 @@ const Footer = () => {
           </div>
         </div>
         <div>
-          <h1 className="text-white text-base flex justify-between">
-            Bantuan <img src="./assets/icons/drop-icon.svg" alt="" />
+          <h1 className="text-white text-base flex justify-between lg:mb-3">
+            Bantuan{" "}
+            <img
+              src="./assets/icons/drop-icon.svg"
+              alt=""
+              className="lg:hidden"
+            />
           </h1>
-          <ul className="text-[#C1C2C4] hidden">
+          <ul className="text-[#C1C2C4] hidden lg:block cursor-pointer">
             <li>FAQ</li>
             <li>Kontak Kami</li>
             <li>Privasi</li>
